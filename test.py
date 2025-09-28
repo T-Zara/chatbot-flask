da44be173cd17abc72df97255a3b1b9fdfaee59a
T-Zara <108338567+T-Zara@users.noreply.github.com>
2025-09-28 23:35:00 +0500

    Made three models today

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 # uncomment to test rule based chatbot
-# from flask import Flask, request, jsonify
-# import requests
-# url = "http://127.0.0.1:5000/chat"
-# while True:
-#     user_input = input("You: ")
-#     if user_input.lower() in ["quit", "break", "end"]:
-#         break
-#     response = requests.post(url, json={"message": user_input})
-#     print("Bot:", response.json()["reply"])
+from flask import Flask, request, jsonify
+import requests
+url = "http://127.0.0.1:5000/chat"
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ["quit", "break", "end"]:
+        break
+    response = requests.post(url, json={"message": user_input})
+    print("Bot:", response.json()["reply"])
 
 # End of testing of rule based chatbot code
